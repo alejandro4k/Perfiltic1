f501811461db7ecf02c1dcced379f845ef00657a
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2020-03-20 01:29:21 -0500

    Updates

---
+++ precache-manifest.98bf51a3fc30d633253fd453fff54092.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60363771f48131103a0c6e5885a2c550",
+    "url": "/Perfiltic1/index.html"
+  },
+  {
+    "revision": "493789372019866a3fb5",
+    "url": "/Perfiltic1/static/css/main.7a3ca686.chunk.css"
+  },
+  {
+    "revision": "6c563f50900c220228e0",
+    "url": "/Perfiltic1/static/js/2.0bfa852b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Perfiltic1/static/js/2.0bfa852b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493789372019866a3fb5",
+    "url": "/Perfiltic1/static/js/main.7ba1cfa8.chunk.js"
+  },
+  {
+    "revision": "717647bba3ca58df5875",
+    "url": "/Perfiltic1/static/js/runtime-main.10883f9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Perfiltic1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "caeb50510df4c248cfba8be6f2588086",
+    "url": "/Perfiltic1/static/media/logoperfiltic.caeb5051.png"
+  }
+]);
